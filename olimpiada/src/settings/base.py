55dae81b94186a457ee4ce6d629b6746f5728a6f
pantelis.simeonidis <pantelisdio@gmail.com>
2024-05-21 17:08:37 +0300

    minor changes

--- olimpiada/src/settings/base.py
+++ olimpiada/src/settings/base.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -168,3 +169,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
